1f93340c3c3393902f29a9acb2c472c3ea9d7485
Célio Benjamim <celiolimabenjamim@gmail.com>
2026-04-11 19:04:39 -0400

    reafctor: modifica a lógica do Wifi para usar as funções builtin do WiFi manager, possibilitando uma configuração mais uniforme

--- esp/Wifi.h
+++ esp/Wifi.h
@@ -1,34 +1,16 @@
 #ifndef WIFI_H
 #define WIFI_H
 
-#include <ESP8266WiFi.h> // WiFi
-#include <ESP8266mDNS.h> // multi DNS
-#include <WiFiManager.h>
-#include <ESP8266WebServer.h>
-#include <EEPROM.h>
+#include <Arduino.h>
 
-class WifiController
-{
-private:
-  ESP8266WebServer server;
-  String id;
-
-public:
-  WifiController(); // Constructor
-  ~WifiController();
-
-  String ip = "";
-  String serverIP = "";
-
-  void startWiFi(String id);
-  void startMDNS(String mdnsName);
-  void updateMDNS();
-  
-  void startWebServer(String id);
-  void handleWebServer();
-
-  void loadServerIP();
-  void saveServerIP(String newIP);
+class Wifi {
+  public:
+    void setup(String idRobo);
+    void loop();
+    String getServerIP();
+    String getIP();
 };
+
+extern Wifi wifi; 
 
 #endif
